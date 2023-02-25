8b3074ac5b691722bdc440463877e6588e29de90
OscarAraya18 <osaronaragar15@gmail.com>
2023-02-25 13:42:58 -0600

    Update

--- backend.js
+++ backend.js
@@ -390,7 +390,7 @@ function createCart(response){
                                      cartCreationHour: currentDate.getHours(),
                                      cartCreationMinute: currentDate.getMinutes()};
     saveDatabase(cartDatabaseLocation, cartDatabase);
-    var responseMessage = {error: false, responseID: 7};
+    var responseMessage = {createdCartCode: createdCartCode, error: false, responseID: 7};
     response.end(JSON.stringify(responseMessage));
 };
 function deleteCart(requestQuery, response){
