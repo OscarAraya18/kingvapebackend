57d8d99775b8548b383e16755eeb4d12dab5c0e7
OscarAraya18 <osaronaragar15@gmail.com>
2023-11-28 17:38:15 -0600

    Audio

--- agentsManagementFunctions.js
+++ agentsManagementFunctions.js
@@ -164,8 +164,8 @@ module.exports = {
         var agentsDatabase = databaseManagementFunctions.readDatabase(constants.routes.agentsDatabase);
         var conversationsDatabase = databaseManagementFunctions.readDatabase(constants.routes.conversationsDatabase);
         const activeConversationIndex = agentsDatabase[previousAgentID].agentActiveConversations.indexOf(activeConversationID);
-        if (activeConversationIndex !== -1) {
-            agentsDatabase[previousAgentID].agentActiveConversations.splice(activeConversationIndex, 1);
+        if (activeConversationIndex != -1) {
+          agentsDatabase[previousAgentID].agentActiveConversations.splice(activeConversationIndex, 1);
         }
         agentsDatabase[newAgentID].agentActiveConversations.push(activeConversationID);
         conversationsDatabase[activeConversationID].assignedAgentID = newAgentID;
