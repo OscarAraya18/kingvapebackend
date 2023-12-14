d3f88a669cc7a995f9e28df3f3fd1ae415b820a7
OscarAraya18 <osaronaragar15@gmail.com>
2023-12-14 10:51:55 -0600

    jueves

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ const WebSocket = require('ws');
 
 const backendHttpRequestServer = express();
 backendHttpRequestServer.use(cors());
-backendHttpRequestServer.use(express.json({limit: '50mb'}));
-
-
 backendHttpRequestServer.use(function(httpRequest, httpResponse, next) {
   httpResponse.setHeader('Access-Control-Allow-Origin', '*');
   httpResponse.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  httpResponse.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  httpResponse.setHeader('Access-Control-Allow-Credentials', true);
+  httpResponse.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
   next();
 });
+backendHttpRequestServer.use(express.json({limit: '50mb'}));
+
 
 const server = backendHttpRequestServer.listen(constants.backendHttpRequestServerConnectionPort);
 module.exports = server;
