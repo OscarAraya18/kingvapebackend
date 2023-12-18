f842a0859d0a5f9d277d455eae89baa2387a4d09
OscarAraya18 <osaronaragar15@gmail.com>
2023-12-18 13:38:29 -0600

    sabado

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ backendHttpRequestServer.use(function(httpRequest, httpResponse, next) {
   next();
 });
 
+backendHttpRequestServer.all('*', function(httpRequest, httpResponse, next) {
+    var responseSettings = {
+      "AccessControlAllowOrigin": httpRequest.headers.origin,
+      "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+      "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+      "AccessControlAllowCredentials": true
+  };
+
+  httpResponse.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  httpResponse.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+  httpResponse.header("Access-Control-Allow-Headers", (httpRequest.headers['access-control-request-headers']) ? httpRequest.headers['access-control-request-headers'] : "x-requested-with");
+  httpResponse.header("Access-Control-Allow-Methods", (httpRequest.headers['access-control-request-method']) ? httpRequest.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+  if ('OPTIONS' == httpRequest.method) {
+    httpResponse.send(200);
+  }
+  else {
+      next();
+  }
+});
+
 backendHttpRequestServer.use(express.json({limit: '50mb'}));
 
 
@@ -25,6 +46,8 @@ const backendAgentHttpRequestServer = require('./agentModule/agentServer.js');
 const backendContactHttpRequestServer = require('./contactModule/contactServer.js');
 const backendWhatsappHttpRequestServer = require('./whatsappModule/whatsappServer.js');
 const backendStoreHttpRequestServer = require('./storeModule/storeServer.js');
+
+
 
 backendHttpRequestServer.use(backendAgentHttpRequestServer);
 backendHttpRequestServer.use(backendContactHttpRequestServer);
