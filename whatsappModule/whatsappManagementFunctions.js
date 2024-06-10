5ebe9b402d21d5a91f3e989ba275f7acb749892d
Oscar Araya Garbanzo <osaronaragar15@gmail.com>
2024-06-10 17:48:10 -0600

    bloque de maria

--- whatsappModule/whatsappManagementFunctions.js
+++ whatsappModule/whatsappManagementFunctions.js
@@ -745,7 +745,7 @@ module.exports = {
       if (httpRequest['body']['entry'][0]['changes'][0]['value']['messages'][0].type != 'reaction'){
         const whatsappConversationRecipientPhoneNumber = httpRequest['body']['entry'][0]['changes'][0]['value']['messages'][0]['from'];
         
-        const blockedNumbers = ['50683315852', '50663768669', '50672015751', '50670931575', '50683392320', '50685178027'];
+        const blockedNumbers = ['50683315852', '50663768669', '50672015751', '50670931575', '50683392320', '50685178027', '50664801051'];
 
         if (!(whatsappConversationRecipientPhoneNumber in blockedNumbers)){   
                
